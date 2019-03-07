f654ab12246bc667ce2fc03d502e9cb52caa1420
118453154 <118453154@BISB45VL.bis.ad.ucc.ie>
2019-03-07 22:14:17 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Homepage</title>
+    <style>
+        h1 {
+text-align: center;
+}
+       body{
+background-color: lightseagreen;
+}
+        </style>
+    </head>
+    
+    <body>
+        <h1>Brian Freeman Homepage</h1>
+        <br><br>      
+        <a href="index.html" target="_blank"><h1><button type="button" style="width:200px;height:120px;color:Red;">Curriculum Vitae</button></h1></a>
+        <br><br>
+        <a href="tiltedpage_scroll_demo.html" target="_blank"><h1><button type="button" style="width:200px;height:120px;color:Red;">My Interests</button></h1></a>
+        <br><br>
+        <a href="eBusiness1.php" target="_blank"><h1><button type="button" style="width:200px;height:120px;color:Red;">Strategic Consulting Services</button></h1></a>
+    
+
+</body>
+</html>
